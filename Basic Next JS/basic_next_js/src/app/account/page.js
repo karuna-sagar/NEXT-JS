84bb09c6df35539efa8cf1cabeb2a6611b4f1795
Karuna sagar <karunasagar129130@gmail.com>
2024-06-12 15:56:50 +0530

    Learn about redirect to some page using navigation

--- Basic Next JS/basic_next_js/src/app/account/page.js
+++ Basic Next JS/basic_next_js/src/app/account/page.js
@@ -1,5 +1,12 @@
+import { redirect } from "next/navigation"
+
 export default function accountInfo() {
+    const userProfile = null;
+    if (userProfile === null) {
+        redirect('profile')
+    }
     return (
+
         <div><h1>this is accountInfo page</h1></div>
     )
 }
