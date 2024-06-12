d021bde2ff10561fb8e05ee96b6b988e04078859
Karuna sagar <karunasagar129130@gmail.com>
2024-06-12 17:31:54 +0530

    Loading file

--- Basic Next JS/basic_next_js/src/app/layout.js
+++ Basic Next JS/basic_next_js/src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
+import loading from "./loading";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Suspense fallback={<Loading />}>{children}</Suspense>
+      </body>
     </html>
   );
 }
---
+++ Basic Next JS/basic_next_js/src/app/loading.js
@@ -0,0 +1,5 @@
+export default function Loading() {
+    return <div>
+        <h1 className="text-5xl">Loading! Please Wait</h1>
+    </div>
+}
