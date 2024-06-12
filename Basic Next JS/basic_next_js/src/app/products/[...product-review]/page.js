5ff8f2d007cb3a54e542f95cb585ee318156f7aa
Karuna sagar <karunasagar129130@gmail.com>
2024-06-12 16:18:08 +0530

    learn for to fetch data

--- Basic Next JS/basic_next_js/src/app/products/[...product-review]/page.js
+++ Basic Next JS/basic_next_js/src/app/products/[...product-review]/page.js
@@ -1,4 +1,5 @@
-export default function productReview() {
+export default function productReview({ params }) {
+    console.log(params)
     return (
         <div>
             <h1> This is the product review page</h1>
