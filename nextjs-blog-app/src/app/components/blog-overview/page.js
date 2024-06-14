23e282949b99d685fa07e7696d2a1d62523bd279
Karuna sagar <karunasagar129130@gmail.com>
2024-06-15 02:01:10 +0530

    "Added dialog component for creating new blog posts to BlogOverview page" Creating  Add Blog

--- nextjs-blog-app/src/app/components/blog-overview/page.js
+++ nextjs-blog-app/src/app/components/blog-overview/page.js
@@ -1,5 +1,64 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 export default function BlogOverview() {
+    const [openDialog, setOpenDialog] = useState(false);
     return (
-        <div>This is the blogDetail page</div>
-    )
+        <div className="min-h-screen flex flex-col gap-10  bg-gradient-to-r from-purple-500 to-blue-600 p-6">
+            <div>
+                <Button onClick={() => setOpenDialog(true)}>Add New Blog</Button>
+            </div>
+            <div>Blog List Section</div>
+            <Dialog
+                open={openDialog}
+                onOpenChange={setOpenDialog}
+            >
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Add New Blog</DialogTitle>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label
+                                htmlFor="name"
+                                className="text-right"
+                            >
+                                Title
+                            </Label>
+                            <Input
+                                id="title"
+                                className="col-span-3"
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label
+                                htmlFor="name"
+                                className="text-right"
+                            >
+                                Description
+                            </Label>
+                            <Input
+                                id="description"
+                                className="col-span-3"
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="button">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 }
